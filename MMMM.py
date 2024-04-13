57dad9c15829487a04991b91a3b7968e9a9fc84b
Michael Järvå <michael.jarva@gmail.se>
2024-04-13 17:37:11 +0200

    chaos weight adjust

--- MMMM.py
+++ MMMM.py
@@ -549,7 +549,7 @@ def main():
         'notslow': {'min_length': -2, 'max_length': 5, 'min_execution': -2, 'max_execution': 4, 'min_familiarity': 2, 'max_familiarity': 15, 'min_variance': -5, 'max_variance': 5, 'min_items': 0, 'max_items': 3},
         'complex': {'min_length': 3, 'max_length': 12, 'min_execution': 0, 'max_execution': 6, 'min_familiarity': 8, 'max_familiarity': 20, 'min_variance': -8, 'max_variance': 5, 'min_items': 0, 'max_items': 3},
         'ordeal': {'min_length': 13, 'max_length': 25, 'min_execution': 4, 'max_execution': 11, 'min_familiarity': 16, 'max_familiarity': 30, 'min_variance': -8, 'max_variance': 5, 'min_items': 0, 'max_items': 2},
-        'chaos': {'min_length': -100, 'max_length': 100, 'min_execution': -100, 'max_execution': 100, 'min_familiarity': -100, 'max_familiarity': 100, 'min_variance': -100, 'max_variance': 100, 'min_items': 0, 'max_items': 8},
+        'chaos': {'min_length': -100, 'max_length': 100, 'min_execution': -100, 'max_execution': 100, 'min_familiarity': -100, 'max_familiarity': 100, 'min_variance': -100, 'max_variance': 100, 'min_items': 3, 'max_items': 8},
         'volatility': {'min_length': -100, 'max_length': 100, 'min_execution': -100, 'max_execution': 100, 'min_familiarity': -100, 'max_familiarity': 100, 'min_variance': 10, 'max_variance': 100, 'min_items': 0, 'max_items': 8}
     }
 
